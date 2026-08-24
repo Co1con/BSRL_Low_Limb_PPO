fa98e4ccfb7687d108cfcd98d1b606437cb725b9
Serein <xie2394805075@163.com>
2026-08-24 20:01:20 +0800

    [feat] 同步修改了bsrl配置的电机层

--- source/BSRL_Low_Limb_PPO/BSRL_Low_Limb_PPO/assets/bsrl.py
+++ source/BSRL_Low_Limb_PPO/BSRL_Low_Limb_PPO/assets/bsrl.py
@@ -1,22 +1,26 @@
 import os  # noqa: I001
 
 import isaaclab.sim as sim_utils
-from isaaclab.actuators import IdealPDActuatorCfg, ImplicitActuatorCfg, DCMotorCfg
 from isaaclab.assets.articulation import ArticulationCfg
 from isaaclab.utils import configclass
 
 from BSRL_Low_Limb_PPO.assets.delayed_implicit_actuator import DelayedImplicitActuatorCfg
-# BSRL_Low_Limb_PPO
 
 BSRL_MODEL_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "bsrl"))
 
-# 机器人默认高度
-BSRL_DEFAULT_ROOT_HEIGHT = 0.8635
-# BSRL_DEFAULT_ROOT_HEIGHT = 0.8665
+# GMR walk1 第一帧左右腿平均并按 0.1 rad 对称化后的贴地站姿高度。
+BSRL_DEFAULT_ROOT_HEIGHT = 0.8665
 
 # 训练时为每个环境随机采样 0~4 个物理步的执行器命令延迟。
 BSRL_ACTUATOR_MIN_DELAY = 0
-BSRL_ACTUATOR_MAX_DELAY = 2
+BSRL_ACTUATOR_MAX_DELAY = 4
+
+# Gearbox-output limits from the SETZ motor datasheet. The small motor is the
+# faster SETZ70-1EB-J; the large motor is the higher-torque SETZ90-1FF-J.
+BSRL_SMALL_MOTOR_PEAK_TORQUE = 112.0
+BSRL_SMALL_MOTOR_PEAK_SPEED = 250.0 * 2.0 * 3.141592653589793 / 60.0
+BSRL_LARGE_MOTOR_PEAK_TORQUE = 216.0
+BSRL_LARGE_MOTOR_PEAK_SPEED = 144.5 * 2.0 * 3.141592653589793 / 60.0
 
 BSRL_ACTION_SCALE_MULTIPLIER = {
     "joint_.*_hip_pitch": 0.25,
@@ -48,7 +52,7 @@ BSRL_CFG = BSRLArticulationCfg(
             max_depenetration_velocity=1.0,
         ),
         articulation_props=sim_utils.ArticulationRootPropertiesCfg(
-            enabled_self_collisions=False,
+            enabled_self_collisions=True,
             solver_position_iteration_count=8,
             solver_velocity_iteration_count=4,
         ),
@@ -58,90 +62,63 @@ BSRL_CFG = BSRLArticulationCfg(
         joint_pos={
             "joint_.*_hip_yaw": 0.0,
             "joint_.*_hip_roll": 0.0,
-            "joint_.*_hip_pitch": -0.1,
+            "joint_.*_hip_pitch": -0.2,
             "joint_.*_knee_pitch": 0.4,
-            "joint_.*_ankle_pitch": -0.3,
+            "joint_.*_ankle_pitch": -0.2,
             "joint_.*_ankle_roll": 0.0,
-
-            # "joint_.*_hip_yaw": 0.0,
-            # "joint_.*_hip_roll": 0.0,
-            # "joint_.*_hip_pitch": -0.2,
-            # "joint_.*_knee_pitch": 0.4,
-            # "joint_.*_ankle_pitch": -0.2,
-            # "joint_.*_ankle_roll": 0.0,
         },
         joint_vel={".*": 0.0},
     ),
     actuators={
-        "legs": DelayedImplicitActuatorCfg(
+        "large_motors": DelayedImplicitActuatorCfg(
             min_delay=BSRL_ACTUATOR_MIN_DELAY,
             max_delay=BSRL_ACTUATOR_MAX_DELAY,
             joint_names_expr=[
                 "joint_.*_hip_pitch",
                 "joint_.*_hip_roll",
-                "joint_.*_hip_yaw",
                 "joint_.*_knee_pitch",
             ],
-            effort_limit_sim=1000.0,
-            velocity_limit_sim=20.0,
+            effort_limit=BSRL_LARGE_MOTOR_PEAK_TORQUE,
+            effort_limit_sim=BSRL_LARGE_MOTOR_PEAK_TORQUE,
+            velocity_limit=BSRL_LARGE_MOTOR_PEAK_SPEED,
+            velocity_limit_sim=BSRL_LARGE_MOTOR_PEAK_SPEED,
             armature=0.01,
             stiffness={
-                "joint_.*_hip_pitch": 300,
-                "joint_.*_hip_roll": 200,
-                "joint_.*_hip_yaw": 200,
-                "joint_.*_knee_pitch": 450,
+                "joint_.*_hip_pitch": 100,
+                "joint_.*_hip_roll": 100,
+                "joint_.*_knee_pitch": 150,
             },
             damping={
-                "joint_.*_hip_pitch": 20,
-                "joint_.*_hip_roll": 15,
-                "joint_.*_hip_yaw": 15,
-                "joint_.*_knee_pitch": 25,
+                "joint_.*_hip_pitch": 2,
+                "joint_.*_hip_roll": 2,
+                "joint_.*_knee_pitch": 4,
             },
         ),
-        "feet": DelayedImplicitActuatorCfg(
+        # SETZ70-1EB-J (16:1): hip yaw and both ankle axes.
+        "small_motors": DelayedImplicitActuatorCfg(
             min_delay=BSRL_ACTUATOR_MIN_DELAY,
             max_delay=BSRL_ACTUATOR_MAX_DELAY,
             joint_names_expr=[
+                "joint_.*_hip_yaw",
                 "joint_.*_ankle_roll",
                 "joint_.*_ankle_pitch",
             ],
-            effort_limit_sim=1000.0,
-            velocity_limit_sim=20.0,
+            effort_limit=BSRL_SMALL_MOTOR_PEAK_TORQUE,
+            effort_limit_sim=BSRL_SMALL_MOTOR_PEAK_TORQUE,
+            velocity_limit=BSRL_SMALL_MOTOR_PEAK_SPEED,
+            velocity_limit_sim=BSRL_SMALL_MOTOR_PEAK_SPEED,
             armature=0.01,
             stiffness={
-                "joint_.*_ankle_roll": 100,
-                "joint_.*_ankle_pitch": 100,
+                "joint_.*_hip_yaw": 100,
+                "joint_.*_ankle_roll": 40,
+                "joint_.*_ankle_pitch": 40,
             },
             damping={
-                "joint_.*_ankle_roll": 8,
-                "joint_.*_ankle_pitch": 8,
+                "joint_.*_hip_yaw": 2,
+                "joint_.*_ankle_roll": 2,
+                "joint_.*_ankle_pitch": 2,
             },
         ),
-        # 原生Implicit actuator写法
-        # "hip_motors": ImplicitActuatorCfg(
-        #     joint_names_expr=["joint_.*_hip_.*"],
-        #     effort_limit_sim=1000.0,
-        #     velocity_limit_sim=10.0,
-        #     stiffness=100.0,
-        #     damping=2.0,
-        #     armature=0.01,
-        # ),
-        # "knee_motors": ImplicitActuatorCfg(
-        #     joint_names_expr=["joint_.*_knee_pitch"],
-        #     effort_limit_sim=1000.0,
-        #     velocity_limit_sim=10.0,
-        #     stiffness=150.0,
-        #     damping=4.0,
-        #     armature=0.01,
-        # ),
-        # "ankle_motors": ImplicitActuatorCfg(
-        #     joint_names_expr=["joint_.*_ankle_.*"],
-        #     effort_limit_sim=1000.0,
-        #     velocity_limit_sim=10.0,
-        #     stiffness=40.0,
-        #     damping=2.0,
-        #     armature=0.01,
-        # ),
     },
     joint_sdk_names=[
         "joint_right_hip_yaw",
