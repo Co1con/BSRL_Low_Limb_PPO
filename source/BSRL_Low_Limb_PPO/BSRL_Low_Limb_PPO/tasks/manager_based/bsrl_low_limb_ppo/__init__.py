8a598855c69eeb582b13ae96ff5bed201ae3775d
serein <xie2394805075@163.com>
2026-09-02 17:07:44 +0800

    [feat] 完成一版基于Hopf步态生成器+RL软约束的拟人步态训练

--- source/BSRL_Low_Limb_PPO/BSRL_Low_Limb_PPO/tasks/manager_based/bsrl_low_limb_ppo/__init__.py
+++ source/BSRL_Low_Limb_PPO/BSRL_Low_Limb_PPO/tasks/manager_based/bsrl_low_limb_ppo/__init__.py
@@ -62,24 +62,3 @@ gym.register(
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
     },
 )
-
-# hopf —— residual learning
-gym.register(
-    id="bsrl-hopf-res-velocity-train",
-    entry_point="isaaclab.envs:ManagerBasedRLEnv",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": f"{__name__}.bsrl_hopf_res_velocity_env_cfg:RobotHopfResEnvCfg",
-        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
-    },
-)
-
-gym.register(
-    id="bsrl-hopf-res-velocity-play",
-    entry_point="isaaclab.envs:ManagerBasedRLEnv",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": f"{__name__}.bsrl_hopf_res_velocity_env_cfg:RobotHopfResPlayEnvCfg",
-        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
-    },
-)
