43f63100aaa140f3c542e4c340e25669835aae7d
serein <xie2394805075@163.com>
2026-09-12 22:57:53 +0800

    [tmp]

--- source/BSRL_Low_Limb_PPO/BSRL_Low_Limb_PPO/tasks/manager_based/bsrl_low_limb_ppo/bsrl_hopf_velocity_env_cfg.py
+++ source/BSRL_Low_Limb_PPO/BSRL_Low_Limb_PPO/tasks/manager_based/bsrl_low_limb_ppo/bsrl_hopf_velocity_env_cfg.py
@@ -120,7 +120,7 @@ class HopfRewardsCfg:
     # Joints
     joint_vel = RewTerm(func=mdp.joint_vel_l2, weight=-0.001)
     joint_acc = RewTerm(func=mdp.joint_acc_l2, weight=-2.5e-7)
-    action_rate = RewTerm(func=mdp.action_rate_l2, weight=-0.2)
+    action_rate = RewTerm(func=mdp.action_rate_l2, weight=-0.1)
     dof_pos_limits = RewTerm(func=mdp.joint_pos_limits, weight=-5.0)
     energy = RewTerm(func=mdp.energy, weight=-2e-5)
     joint_deviation_hips = RewTerm(
